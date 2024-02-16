41c8b905c832a9b89e9d5ddf5847b6be6a88ee7e
Santi <chesslike693@gmail.com>
2024-02-16 03:01:23 -0300

    Responsive page

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 $(document).ready(function(){
-      // Al cargar la página, ocultamos las cortinas
-      $('.left-curtain').css('width', '0%');
-      $('.right-curtain').css('width', '0%');
-      $("#taytay").hide();
-    
-      $('.valentines-day').click(function(){
-        // Animación de desvanecimiento de los elementos del sobre
-        $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
-        $('.envelope').fadeOut(800, function() {
-          // Ocultar elementos dentro de .valentines-day
-          $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
-          
-    
-          // Hacer visible la carta con una animación ondulante
-          $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
-          $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
-            var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
-            $(this).css('transform', 'scale(' + scale + ')');
-          $("#taytay").show();
-          var audioElement = $("#heaven")[0];
-        audioElement.currentTime = 50; // Inicia la reproducción a los 50 segundos
-        audioElement.play(); 
-          }}); // Animación de ondulación
-        });
-      });
+  // Al cargar la página, ocultamos las cortinas
+  $('.cortina-izquierda').css('width', '0%');
+  $('.cortina-derecha').css('width', '0%');
+  $("#taytay").hide();
+
+  $('.dia-de-san-valentin').click(function(){
+    // Animación de desvanecimiento de los elementos del sobre
+    $('.sobre').css({'animation':'caida 3s linear 1', '-webkit-animation':'caida 3s linear 1'});
+    $('.sobre').fadeOut(800, function() {
+      // Ocultar elementos dentro de .dia-de-san-valentin
+      $('.dia-de-san-valentin .corazon, .dia-de-san-valentin .texto, .dia-de-san-valentin .frente').hide();
+      
+
+      // Hacer visible la carta con una animación ondulante
+      $('#carta').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
+      $('#carta').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
+        var escala = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
+        $(this).css('transform', 'scale(' + escala + ')');
+      $("#taytay").show();
+      var audioElement = $("#cielo")[0];
+    audioElement.currentTime = 50; // Inicia la reproducción a los 50 segundos
+    audioElement.play(); 
+      }}); // Animación de ondulación
     });
-    
-    document.addEventListener('mousemove', function(e){
-      let container = document.getElementById('container');
-      let heart = document.createElement('span');
-      let x = e.offsetX;
-      let y = e.offsetY;         
-      heart.style.left = x+'px';
-      heart.style.top = y+'px';
-      
-      let size = Math.random() * 80;
-      heart.style.width = 20 + size+'px';
-      heart.style.height = 20 + size+'px';
+  });
+});
+
+document.addEventListener('mousemove', function(e){
+  let contenedor = document.getElementById('contenedor');
+  let corazon = document.createElement('span');
+  let x = e.offsetX;
+  let y = e.offsetY;         
+  corazon.style.left = x+'px';
+  corazon.style.top = y+'px';
   
-      let transformValue = Math.random() * 360;
-      heart.style.transform = 'rotate('+transformValue+'deg)';
-      
-      container.appendChild(heart);
+  let tamaño = Math.random() * 80;
+  corazon.style.width = 20 + tamaño+'px';
+  corazon.style.height = 20 + tamaño+'px';
+
+  let valorTransformacion = Math.random() * 360;
+  corazon.style.transform = 'rotate('+valorTransformacion+'deg)';
   
-      setTimeout(function(){
-        heart.remove();
-      },1000)
-    })
+  contenedor.appendChild(corazon);
+
+  setTimeout(function(){
+    corazon.remove();
+  },1000)
+})
