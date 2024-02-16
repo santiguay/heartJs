9ad0faaeb3d1b269851ad6fd4b5cb0f370cbd248
Santi <chesslike693@gmail.com>
2024-02-16 02:39:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+      // Al cargar la página, ocultamos las cortinas
+      $('.left-curtain').css('width', '0%');
+      $('.right-curtain').css('width', '0%');
+      $("#taytay").hide();
+    
+      $('.valentines-day').click(function(){
+        // Animación de desvanecimiento de los elementos del sobre
+        $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
+        $('.envelope').fadeOut(800, function() {
+          // Ocultar elementos dentro de .valentines-day
+          $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+          
+    
+          // Hacer visible la carta con una animación ondulante
+          $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
+          $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
+            var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
+            $(this).css('transform', 'scale(' + scale + ')');
+          $("#taytay").show();
+          var audioElement = $("#heaven")[0];
+        audioElement.currentTime = 50; // Inicia la reproducción a los 50 segundos
+        audioElement.play(); 
+          }}); // Animación de ondulación
+        });
+      });
+    });
+    
+    document.addEventListener('mousemove', function(e){
+      let container = document.getElementById('container');
+      let heart = document.createElement('span');
+      let x = e.offsetX;
+      let y = e.offsetY;         
+      heart.style.left = x+'px';
+      heart.style.top = y+'px';
+      
+      let size = Math.random() * 80;
+      heart.style.width = 20 + size+'px';
+      heart.style.height = 20 + size+'px';
+  
+      let transformValue = Math.random() * 360;
+      heart.style.transform = 'rotate('+transformValue+'deg)';
+      
+      container.appendChild(heart);
+  
+      setTimeout(function(){
+        heart.remove();
+      },1000)
+    })
